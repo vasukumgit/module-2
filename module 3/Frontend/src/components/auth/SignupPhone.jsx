35a5b6e1cfbe641074fde805b392173b7dfb1678
vasukumgit <vasudevanleela25@gmail.com>
2026-07-06 11:35:08 +0530

    Update SignupPhone.jsx

--- module 3/Frontend/src/components/auth/SignupPhone.jsx
+++ module 3/Frontend/src/components/auth/SignupPhone.jsx
@@ -38,7 +38,7 @@ function SignupPhone() {
     }
 
     try {
-      const res = await fetch("http://localhost:5050/api/auth/create-user", {
+      const res = await fetch("/api/auth/create-user", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
