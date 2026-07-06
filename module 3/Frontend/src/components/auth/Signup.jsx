507ce8d82a178d593ed1801c064a7b65ca3d7347
vasukumgit <vasudevanleela25@gmail.com>
2026-07-06 11:33:35 +0530

    Update Signup.jsx

--- module 3/Frontend/src/components/auth/Signup.jsx
+++ module 3/Frontend/src/components/auth/Signup.jsx
@@ -27,7 +27,7 @@ function Signup() {
     };
 
     try {
-      const res = await fetch("http://localhost:5050/api/auth/send-otp", {
+      const res = await fetch("/api/auth/send-otp", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(signupData),
