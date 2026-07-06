0b6435906d288c2964bf8e39a45f4fe2d89661c4
vasukumgit <vasudevanleela25@gmail.com>
2026-07-06 11:56:23 +0530

    Update ChartsDropdown.jsx

--- module 3/Frontend/src/components/editorPanel/components/sidebar-left/object-dropdown/charts-dropdown/ChartsDropdown.jsx
+++ module 3/Frontend/src/components/editorPanel/components/sidebar-left/object-dropdown/charts-dropdown/ChartsDropdown.jsx
@@ -28,7 +28,7 @@ function ChartsDropdown({
   // =========================
   // API BASE URL
   // =========================
-  const API_URL = "http://localhost:5050/api/chart-table";
+  const API_URL = "/api/chart-table";
 
   // =========================
   // STATES
