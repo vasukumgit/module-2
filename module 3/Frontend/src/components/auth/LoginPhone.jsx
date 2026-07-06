01bfa16521bf288459101c594219e8e51a681526
vasukumgit <vasudevanleela25@gmail.com>
2026-07-06 11:23:16 +0530

    Update LoginPhone.jsx

--- module 3/Frontend/src/components/auth/LoginPhone.jsx
+++ module 3/Frontend/src/components/auth/LoginPhone.jsx
@@ -23,7 +23,7 @@ function LoginPhone() {
     setPhoneError("");
 
     try {
-      const res = await fetch("http://localhost:5050/api/auth/send-otp", {
+      const res = await fetch("/api/auth/send-otp", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ phone }),
