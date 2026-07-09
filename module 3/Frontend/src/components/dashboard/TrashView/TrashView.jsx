3df949f1794f86a229fb431ab7da72e854d64121
vasukumgit <vasudevanleela25@gmail.com>
2026-07-09 10:18:38 +0530

    Update TrashView.jsx

--- module 3/Frontend/src/components/dashboard/TrashView/TrashView.jsx
+++ module 3/Frontend/src/components/dashboard/TrashView/TrashView.jsx
@@ -96,7 +96,7 @@ const TrashView = () => {
     try {
       const token = localStorage.getItem("token");
 
-      const res = await axios.get("http://localhost:5050/api/trash", {
+      const res = await axios.get("/api/trash", {
         headers: {
           Authorization: `Bearer ${token}`
         }
@@ -130,7 +130,7 @@ const TrashView = () => {
 
       for (const id of selectedIds) {
         await axios.put(
-          `http://localhost:5050/api/trash/recover/${id}`,
+          `/api/trash/recover/${id}`,
           {},
           {
             headers: {
@@ -155,7 +155,7 @@ const TrashView = () => {
     try {
       const token = localStorage.getItem("token");
 
-      await axios.delete("http://localhost:5050/api/trash", {
+      await axios.delete("/api/trash", {
         headers: {
           Authorization: `Bearer ${token}`
         }
